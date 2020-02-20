7df08aae3c914f1e449a4f898f565f7f5e0df22b
Denys <herasymuk@ucu.edu.ua>
2020-02-20 20:11:05 +0200

    create a google map with markers and popup

--- web_server/map.py
+++ web_server/map.py
@@ -6,11 +6,9 @@ map_page = Blueprint('map', __name__, template_folder='templates')
 
 @map_page.route("/map", methods=["GET", "POST"])
 def map():
-    companies_information = [{"organization_name": "UCU", "lat": 24.023845, "lng": 49.817744,
+    companies_information = [{"organization_name": "UCU", "lat": 49.817731, "lng": 24.023823,
                               "needs": 'meal', "url": 'https://cms.ucu.edu.ua/course/view.php?id=2348'},
-                             {"organization_name": "UCU Colegium", "lat": 25.023845, "lng": 50.817744,
+                             {"organization_name": "UCU Colegium", "lat": 49.818160, "lng": 24.022621,
                               "needs": 'help', "url": 'https://cms.ucu.edu.ua/course/view.php?id=2348'}]
 
-    lat = -25.344
-    lng = 131.036
     return render_template("map.html", coords=companies_information)
