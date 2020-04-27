eab147020b1b9f5c94ebe9807bf3bf16604e32d9
Denys <herasymuk@ucu.edu.ua>
2020-04-27 15:54:43 +0300

    multiple select

--- web_server/receive_form.py
+++ web_server/receive_form.py
@@ -9,26 +9,20 @@ def get_data():
         if request.form.get("address"):
             if request.form.get("city") and request.form.get("time_to")\
                     and request.form.get("time_out") and request.form.get("description"):
-                items = ["address", "city", "time_to", "time_out", "description"]
-                # input_data = {"1": [request.form.get(item) for item in items]}
                 input_data = {'user_id': 123456,
                               'type_id': 123456,
-                              'categories': [],
+                              'categories': request.form.getlist("categories"),
                               'locations': [request.form.get('city'), request.form.get('address')],
                               'time_from': request.form.get('time_to'),
                               'time_to': request.form.get('time_out'),
                               'description': request.form.get('description')}
-                categories = [request.form.get("category1"), request.form.get("category2")]
-                for category in categories:
-                    if category is not None:
-                        input_data['categories'].append(category)
                 print(input_data)
                 return input_data
             else:
-                error = 'enter password'
-                return render_template('add_receive_form.html', error=error)
+                error = 'enter all information'
+                return render_template('add_receive_station.html', error=error)
         else:
-            error = 'enter address'
-            return render_template('add_receive_form.html', error=error)
+            error = 'enter all information'
+            return render_template('add_receive_station.html', error=error)
 
     return render_template("login.html")
