cf845c444947900e7db4b0dc7dc4a4f0afbef796
LAnastasiia <lazorenko@ucu.edu.ua>
2020-04-30 23:21:57 +0300

    fill profile page from db f-n info

--- web_server/profile_org.py
+++ web_server/profile_org.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-
 import database.db
+
 
 profile_org_page = Blueprint('profile_org', __name__, template_folder='templates')
 
@@ -10,10 +10,30 @@ def profile_org():
     if request.method == "POST":
         return "good"
 
-    org = {'org_name': database.db.profile_info['username'], 'org_type': None, 'num_stations': None,
-           'phone_number': None, 'email': database.db.profile_info['email']}
 
-    return render_template("profile_org.html", org=org)
+    #     :param user_id:
+    #     :return: (dict) {'username': str, 'phone_number': str / None, 'email': str,
+    #     'stations': [{'name': str, 'address': str, 'categories': ['food' ... ]}]}
+    #     """
+    print(database.db.profile_info)
+
+    if database.db.profile_info is not None:
+        org_stations = database.db.db.db_station.get_profile_info(database.db.profile_info["user_id"]) or []
+        org_profile = {'org_name': database.db.profile_info['username'],
+                       'org_type': database.db.profile_info['type_id'],
+                       'email': database.db.profile_info.get('email'),
+                       'phone_number': database.db.profile_info.get('phone_number'),
+                       'num_stations': len(org_stations) if org_stations else 0,
+                       }
+        print(database.db.db.db_station.get_profile_info(9))
+        return render_template("profile_org.html", org=org_profile, stations=org_stations)
+
+    return "R"
+
+
+    #
+
+
     # return render_template("profile_org.html", org_name="Myloserd", org_type="Prytuloc",
     #                        num_stations=9, phone_number=999, email="denys@gmail", name_stations=["Prutuloc1",
     #                                                                                              "Prutuloc2"],
