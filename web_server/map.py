ec1b9c2161044e3c768f9c9fa9e0f23adfc5d885
LAnastasiia <lazorenko@ucu.edu.ua>
2020-04-30 22:18:18 +0300

    render map with new markers

--- web_server/map.py
+++ web_server/map.py
@@ -17,7 +17,7 @@ def map():
 
     print("receiver_type_0", request.form)
     if request.method == "POST":
-        db.db_map.get_easy_rcv_station({
+        matched_stations = db.db_map.get_easy_rcv_station({
             'city': request.form.get("city_input"),
             'station_type': request.form.getlist("station_type"),
             'time_from': "01-01-2018",
@@ -26,11 +26,15 @@ def map():
             'categories': request.form.getlist("donation")
         })
 
-    companies_information = [{"organization_name": "UCU", "lat": 49.817731, "lng": 24.023823,
+        return render_template("map.html", coords=matched_stations,
+                               city_center_coords={"lat": 49.8397, "lng": 24.0297},
+                               profile_info=database.db.profile_info)
+
+    sample_stations = [{"organization_name": "UCU", "lat": 49.817731, "lng": 24.023823,
                               "needs": 'meal', "url": 'https://cms.ucu.edu.ua/course/view.php?id=2348'},
                              {"organization_name": "UCU Colegium", "lat": 49.818160, "lng": 24.022621,
                               "needs": 'help', "url": 'https://cms.ucu.edu.ua/course/view.php?id=2348'}]
 
-    return render_template("map.html", coords=companies_information,
+    return render_template("map.html", coords=sample_stations,
                            city_center_coords={"lat": 49.8397, "lng": 24.0297},
                            profile_info=database.db.profile_info)
