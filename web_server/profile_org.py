4471e4c603890cd80c6433f5973b186ef59de1cc
LAnastasiia <lazorenko@ucu.edu.ua>
2020-04-30 23:29:10 +0300

    fill profile page from db f-n info

--- web_server/profile_org.py
+++ web_server/profile_org.py
@@ -5,36 +5,25 @@ import database.db
 profile_org_page = Blueprint('profile_org', __name__, template_folder='templates')
 
 
+def _redirect_url(default='login.login'):
+    """
+    Helper f-n for redirecting back from unauthorized roots.
+    :param default: base root (e.g. app's main page)
+    :return: url for back-redirection
+    """
+    return request.args.get('next') or request.referrer or url_for(default)
+
+
 @profile_org_page.route("/profile_org", methods=["GET", "POST"])
 def profile_org():
     if request.method == "POST":
-        return "good"
-
-
-    #     :param user_id:
-    #     :return: (dict) {'username': str, 'phone_number': str / None, 'email': str,
-    #     'stations': [{'name': str, 'address': str, 'categories': ['food' ... ]}]}
-    #     """
-    print(database.db.profile_info)
-
+        return "?"
     if database.db.profile_info is not None:
         org_stations = database.db.db.db_station.get_profile_info(database.db.profile_info["user_id"]) or []
         org_profile = {'org_name': database.db.profile_info['username'],
                        'org_type': database.db.profile_info['type_id'],
                        'email': database.db.profile_info.get('email'),
                        'phone_number': database.db.profile_info.get('phone_number'),
-                       'num_stations': len(org_stations) if org_stations else 0,
-                       }
-        print(database.db.db.db_station.get_profile_info(9))
+                       'num_stations': len(org_stations) if org_stations else 0}
         return render_template("profile_org.html", org=org_profile, stations=org_stations)
-
-    return "R"
-
-
-    #
-
-
-    # return render_template("profile_org.html", org_name="Myloserd", org_type="Prytuloc",
-    #                        num_stations=9, phone_number=999, email="denys@gmail", name_stations=["Prutuloc1",
-    #                                                                                              "Prutuloc2"],
-    #                        categories=["food", "sleep"], addresses=["UCU", "ucu2"])
+    return redirect(_redirect_url())
